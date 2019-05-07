3e0508cd446f6e6409f776cbc98be2382d9d2ab5
svavar16 <spg009@gmail.com>
2019-05-07 12:40:29 +0000

    added create apartment, delete apartment

--- Apartments/views.py
+++ Apartments/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from Apartments.forms.apartments_form import ApartmentsCreateForm
+from Apartments.models import ApartmentImage, Apartments
 
 
 def index(request):
@@ -9,9 +9,19 @@ def index(request):
 
 def create_apartment(request):
     if request.method == 'POST':
-        print(1)
+        form = ApartmentsCreateForm(data=request.POST)
+        if form.is_valid():
+            apartment = form.save()
+            apartment_image = ApartmentImage(image=request.POST['image'], apartment=apartment)
+            apartment_image.save()
+            return redirect('apartment-index')
     else:
-        print(2)
+        form = ApartmentsCreateForm()
     return render(request, 'apartments/create_apartment.html', {
         'form': form
     })
+
+def delete_apartment(request, id):
+    apartment = get_object_or_404(Apartments, pk=id)
+    apartment.delete()
+    return redirect('apartment-index')
