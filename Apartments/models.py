d50b2b7a0d55be6bb550ae6fb501c0e30b34faef
svavar16 <spg009@gmail.com>
2019-05-07 10:37:29 +0000

    added the classes into models

--- Apartments/models.py
+++ Apartments/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from User.models import Person
 
 # Create your models here.
+
+
+class Location(models.Model):
+    streetName = models.CharField(max_length=255)
+    houseNumber = models.IntegerField(max_length=10)
+    city = models.CharField(max_length=255)
+    postalCode = models.IntegerField(max_length=10)
+
+
+class Apartments(models.Model):
+    price = models.FloatField(max_length=20)
+    size = models.FloatField(max_length=5)
+    locationID = models.ForeignKey(Location, on_delete=models.CASCADE)
+    rooms = models.IntegerField(max_length=3)
+    privateEntrance = models.BooleanField()
+    animalsAllowed = models.BooleanField()
+    garage = models.BooleanField()
+    yearBuild = models.IntegerField()
+    sellerID = models.ForeignKey(Person)
+
+
+class ApartmentImage(models.Model):
+    candyImage = models.CharField(max_length=999)
+    apartmentID = models.ForeignKey(Apartments)
--- User/models.py
+++ User/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from Apartments.models import Location, Apartments
+
 
 # Create your models here.
+
+
+class Person(models.Model):
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    password = models.CharField(max_length=30)
+    email = models.CharField(max_length=255)
+    locationID = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+
+class CardDetails(models.Model):
+    cardNumber = models.IntegerField(max_length=16)
+    dateOfExpire = models.DateField()
+    Owner = models.ForeignKey(Person)
+
+
+class RealtorAgents(models.Model):
+    PersonID = models.ForeignKey(Person)
+    apartmentInChargeOf = models.ForeignKey(Apartments, on_delete=models.CASCADE)
+    dateOfCreation = models.DateField()
+
+
+class Customers(models.Model):
+    PersonID = models.ForeignKey(Person)
+    dateOFCreation = models.DateField()
+
+
+class Transaction(models.Model):
+    apartmentID = models.ForeignKey(Apartments)
+    cardID = models.ForeignKey(CardDetails)
+    customersID = models.ForeignKey(Customers)
+    realtorAgentID = models.ForeignKey(RealtorAgents)
+
+
