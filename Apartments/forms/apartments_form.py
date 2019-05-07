c856869af8f9245cd95f6896eac0c7945ea428b1
svavar16 <spg009@gmail.com>
2019-05-07 11:57:48 +0000

    added the apartment form

---
+++ Apartments/forms/apartments_form.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm, widgets, forms
+from Apartments.models import Apartments
+
+
+class ApartmentsCreateForm(ModelForm):
+    image = forms.CharField(required=True)
+    
+    class Meta:
+        model = Apartments
+        exclude = ['id']
+        widgets = {
+            'price': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'size': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'address': widgets.Select(attrs={'class': 'form-control'}),
+            'rooms': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'privateEntrance': widgets.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'animalsAllowed': widgets.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'garage': widgets.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'yearBuild': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'sellerID': widgets.Select(attrs={'class': 'form-control'}),
+        }
--- Apartments/views.py
+++ Apartments/views.py
@@ -5,3 +5,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'apartments/index.html')
+
+
+def create_apartment(request):
+    if request.method == 'POST':
+        print(1)
+    else:
+        print(2)
+    return render(request, 'apartments/create_apartment.html', {
+        'form': form
+    })
