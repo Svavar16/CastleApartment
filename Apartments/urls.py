e5046997ad118e3287e1921b502a13f8e65f3110
svavar16 <spg009@gmail.com>
2019-05-08 12:24:37 +0000

    erased unused material within the application

--- Apartments/urls.py
+++ Apartments/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="apartment-index"),
-    path('<int:id>', views.apartment_details, name='apartment_details'),
+    path('<int:id>', views.get_apartment_by_id, name='apartment_details'),
     path('create_apartment', views.create_apartment, name='create_apartment'),
     path('delete_apartment/<int:id>', views.delete_apartment, name='delete_apartment'),
-    path('create_location', views.create_location, name='create_location'),
 ]
--- Apartments/views.py
+++ Apartments/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from Apartments.forms.apartments_form import ApartmentsCreateForm, LocationCreateForm
-from Apartments.models import ApartmentImage, Apartments, Location
+from Apartments.models import ApartmentImage, Apartments
 
 
 def index(request):
-    return render(request, 'apartments/index.html')
+    context = {'apartments': Apartments.objects.all()}
+    return render(request, 'apartments/index.html', context)
 
 
-def apartment_details(request, id):
+def get_apartment_by_id(request, id):
     render(request, 'apartments/apartment_details.html', {
-        'apartment': get_object_or_404(Apartments, pk=id)
+        'apartments': get_object_or_404(Apartments, pk=id)
     })
 
 
@@ -39,17 +40,3 @@ def delete_apartment(request, id):
     apartment = get_object_or_404(Apartments, pk=id)
     apartment.delete()
     return redirect('apartment-index')
-
-
-def create_location(request):
-    if request == 'POST':
-        location_form = LocationCreateForm(data=request.POST)
-        if location_form.is_valid():
-            location = location_form.save()
-            location.save()
-            return redirect('create_apartment')
-    else:
-        form = LocationCreateForm()
-    return render(request, 'apartments/create_location.html', {
-        'form': form
-    })
