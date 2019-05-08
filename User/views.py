83ccac88a932e34de9e60aa69e8c3a602692a81a
bjorgvin92 <bjorgvinj17@ru.is>
2019-05-08 12:19:42 +0000

    added fields to user

--- User/views.py
+++ User/views.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from User.forms.create_user import CreateUserForm
 
 
-# Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(data=request.POST)
+        form = CreateUserForm(data=request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
     return render(request, 'user/register.html', {
-        'form': UserCreationForm()
+        'form': CreateUserForm()
     })
