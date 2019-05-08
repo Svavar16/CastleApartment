8fb5fce907528fdfa09575ffc2b493a7745a345a
bjorgvin92 <bjorgvinj17@ru.is>
2019-05-08 14:34:58 +0000

    fixed form for register

--- User/views.py
+++ User/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from User.forms.create_user import CreateUserForm
 
@@ -7,7 +8,13 @@ def register(request):
     if request.method == 'POST':
         form = CreateUserForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            form = form.save(commit=False)
+            user = User.objects.create_user(username=form.username,
+                                            email=form.email,
+                                            password=form.password)
+            user.first_name = form.first_name
+            user.last_name = form.last_name
+            user.save()
             return redirect('login')
     return render(request, 'user/register.html', {
         'form': CreateUserForm()
