00a067a3db855ca2c5d9afe9b5db51d6dc32e678
svavar16 <spg009@gmail.com>
2019-05-09 11:32:44 +0000

    working on arrange by price

--- Apartments/urls.py
+++ Apartments/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="apartment-index"),
     path('all_listing', views.all_listing, name="apartment_all_listing"),
-    path('all_listing/order_by_name', views.all_listing_by_price, name="apartment_all_listing_by_price"),
+    path('all_listing', views.all_listing_by_price, name="apartment_all_listing_by_price"),
     path('<int:id>', views.get_apartment_by_id, name='apartment_details'),
     path('create_apartment', views.create_apartment, name='create_apartment'),
     path('delete_apartment/<int:id>', views.delete_apartment, name='delete_apartment'),
---
+++ static/js/main.js
@@ -0,0 +1,33 @@
+$('#arrange_by_price_btn').on('click', function (e){
+    e.preventDefault();
+    $.ajax({
+        url: 'apartments/all_listing',
+        type: 'GET',
+        success: function (resp) {
+            var newHTML = resp.data.map(d => {
+                return `<div class="card" style="width: 18rem;">
+                            <a href="/apartments/${d.id}">
+                                <img src="${d.firstImage}" class="card-img-top" alt="Apartment Image">
+                                <div class="card-body">
+                                    <h5 class="card-title">${d.locationID.streetName} ${d.locationID.houseNumber} </h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Price: ${d.price}</li>
+                                    <li class="list-group-item">Size: ${d.size}</li>
+                                    <li class="list-group-item">Year Build: ${d.yearBuild}</li>
+                                </ul>
+                                <div class="card-body">
+                                    <a href="${d.id}" class="card-link">More Details</a>
+                                </div>
+                            </a>
+                        </div>`
+            })
+            $('#apartment-card-details').html(newHTML.join(''));
+        },
+        error: function (xhr, status, error) {
+            // todo: show the error
+            console.error(error);
+        }
+    })
+});
