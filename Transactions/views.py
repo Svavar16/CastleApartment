36e0348c2ce5bc3f6874e3ddb33a30b944c73043
bjorgvin92 <bjorgvinj17@ru.is>
2019-05-12 16:35:43 +0000

    added template and adjusted the view so the creditcard passed to the view are only the last 4 digits

--- Transactions/views.py
+++ Transactions/views.py
@@ -3,16 +3,23 @@ from Apartments.models import Apartments
 from django.contrib.auth.models import User
 from User.models import CardDetails
 from Transactions.models import Transactions
+from django.contrib import auth
 
 # Create your views here.
 
 def make_transaction(request, apartment_id):
-    buyer = request.user
+    buyer = auth.get_user(request)
     apartment = get_object_or_404(Apartments, pk=apartment_id)
-    seller = apartment.sellerID
-    credit_card = get_object_or_404(CardDetails, owner=buyer)
-    transaction = Transactions(buyer, seller, credit_card, apartment)
+    seller = User.objects.get(pk=apartment.sellerID.id)
+    credit_card = get_object_or_404(CardDetails, owner=buyer.id)
+    transaction = Transactions(buyer=buyer, seller=seller, payment=credit_card, apartment=apartment)
     transaction.save()
     apartment.sellerID = buyer
     apartment.save()
+    return render(request, 'Transactions/make_transaction.html', {
+        'buyer': buyer,
+        'apartment': apartment,
+        'seller': seller,
+        'credit_card': credit_card.cardNumber[-4:]
+    })
 #muna breyta apartment owner og save-a
